f7cad77f7ba3f9fc20ed69dcb1528fa2ab1b5d5c
Jakub <117046128+KLesiu@users.noreply.github.com>
2023-10-15 00:21:45 +0200

    Try to connect to vercel

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors:true});
-  app.listen("https://todo-list-v2-api.vercel.app/")
+  app.listen(3000)
 }
 bootstrap();
